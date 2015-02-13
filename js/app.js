59a66a8ef8c9c31467d7d7f76a6d58e670ce08b1
Jarold84 <jason.arold@gmail.com>
2015-02-12 17:26:28 -0800

    updated!

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('myApp', ['ngRoute']).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when("/", {templateUrl: "views/main.html", controller: "MainController"}).
+	when("/about", {templateUrl: "views/about.html", controller: "MainController"}).
+	when("/contact", {templateUrl: "views/contact.html", controller: "MainController"}).
+	when("/portfolio", {templateUrl: "views/portfolio.html", controller: "MainController"}).
+	when("/resume", {templateUrl: "views/resume.html", controller: "MainController"}).
+	otherwise({
+	    redirectTo: '/'});
+}]);
---
+++ js/controllers/MainController.js
@@ -0,0 +1,4 @@
+app.controller('MainController', ['$scope', function($scope) {
+    console.log("inside controller");
+    $scope.testText = "Blah blah blah";
+}]);
